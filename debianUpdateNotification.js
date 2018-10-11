04f7b9fc70ddd6618c21a8dc13ab173ae845a011
Max Hauser <moritz.heusinger@gmail.com>
2018-10-11 12:20:18 +0200

    Create debianUpdateNotification.js
    -script checks for apt updates on a linux system
    -if new updates available it will send to receiver mail

---
+++ debianUpdateNotification.js
@@ -0,0 +1,34 @@
+//  Debian Package Notification Script
+/*
+*   @author Moritz Heusinger <moritz.heusinger@gmail.com>
+*   The scripts checks for apt upgrades, everyday at 06:00 p.m., if there are upgrades
+*   available it will send an E-Mail to the configured receiverMail.
+*
+*   Requirement: An active instance of the Mail adapter
+*
+*   logging:        if true, information will be logged
+*   senderMail:     mail address of the sender
+*   receiverMail:   target mail address
+*/
+
+const logging = true;
+
+const senderMail = 'john.doe@femail.com';
+const receiverMail = 'jane.doe@mail.com';
+
+schedule('0 18 * * *', () => {
+    exec('apt update >/dev/null && apt list --upgradeable', (err, stdout, stderr) => {
+        let upgradeable = stdout.split('...')[1];
+        if(upgradeable.length <= 8) upgradeable = false; 
+        if(logging) log('Checking for updates via apt ...', 'info');
+        if(upgradeable) {
+            if(logging) log('The following upgrades are available:\n' + upgradeable, 'info');
+             sendTo('email', {
+                    from:    senderMail,
+                    to:      receiverMail,
+                    subject: 'Es sind neue Updates für deinen Rock64 verfügbar',
+                    text:    'Die folgenden Updates sind verfügbar:\n' + upgradeable
+             });
+        } else if(logging) log('No new updates available', 'info'); // endIf
+    });
+});
