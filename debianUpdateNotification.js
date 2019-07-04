6e13ff41af3b32e6031eac2d75cda306219a47b6
Max Hauser <moritz.heusinger@gmail.com>
2019-07-04 22:10:01 +0200

    Changes if someone ioBroker not running as root
    - when apt update not working try sudo apt update
    - note you will need to allow it in a custom /etc/sudoers.d/ file

--- debianUpdateNotification.js
+++ debianUpdateNotification.js
@@ -1,10 +1,8 @@
 //  Debian Package Notification Script
 /*
 *   @author Moritz Heusinger <moritz.heusinger@gmail.com>
-*   The scripts checks for apt upgrades, everyday at 06:00 p.m., if there are upgrades
-*   available it will send an E-Mail to the configured receiverMail.
-*
-*   Requirement: An active instance of the Mail adapter
+*   The script checks for apt upgrades, everyday at 06:00 p.m., if there are  new
+*   upgrades available it will send an E-Mail to the configured receiverMail.
 *
 *   logging:        if true, information will be logged
 *   senderMail:     mail address of the sender
@@ -13,22 +11,36 @@
 
 const logging = true;
 
-const senderMail = 'john.doe@femail.com';
-const receiverMail = 'jane.doe@mail.com';
+const senderMail = 'moritz-iobroker@web.de';
+const receiverMail = 'moritz.heusinger@gmail.com';
 
-schedule('0 18 * * *', () => {
-    exec('apt update >/dev/null && apt list --upgradeable', (err, stdout, stderr) => {
-        let upgradeable = stdout.split('...')[1];
-        if(upgradeable.length <= 8) upgradeable = null; 
-        if(logging) log('Checking for updates via apt ...', 'info');
-        if(upgradeable) {
-            if(logging) log('The following upgrades are available:\n' + upgradeable, 'info');
-             sendTo('email', {
-                    from:    senderMail,
-                    to:      receiverMail,
-                    subject: 'Es sind neue Updates für deinen Rock64 verfügbar',
-                    text:    'Die folgenden Updates sind verfügbar:\n' + upgradeable
-             });
-        } else if(logging) log('No new updates available', 'info');
-    });
+createState('javascript.0.system.debianUpgradeable', {
+    type: 'string',
+    write: true,
+    read: true
 });
+
+    exec('apt update >/dev/null || sudo apt update >/dev/null && apt list --upgradeable', (err, stdout, stderr) => {
+        let upgradeable = stdout.split('...')[1];
+        if (logging) log('Checking for updates via apt ...', 'info');
+        if (stderr && !stdout) return log(`Error checking for updates via apt: ${stderr}`, `error`);
+        if (upgradeable.length <= 8) upgradeable = null;
+        if (upgradeable) {
+            let upgradeableArrayString = JSON.stringify(upgradeable.split('\n').filter(element => element !== ''));
+            if(getState('javascript.0.system.debianUpgradeable').val !== upgradeableArrayString) {
+                setState('javascript.0.system.debianUpgradeable', upgradeableArrayString, true);
+                if(logging) log('The following upgrades are available:\n' + upgradeable, 'info');
+                 sendTo('email', {
+                        from:    senderMail,
+                        to:      receiverMail,
+                        subject: 'Es sind neue Updates für deinen Rock64 verfügbar',
+                        text:    `Die folgenden Updates sind verfügbar:\n ${upgradeable}`
+                 });
+            } else {
+                if (logging) log('There are available dbpkg updates, but non of them are new.', 'info');
+            } // endElse
+        } else {
+            if(logging) log('No new updates available', 'info');
+            setState('javascript.0.system.debianUpgradeable', '[]', true);
+        } // endElse
+    });
